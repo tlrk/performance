4b1f0d6fa45285eab9a13d208e1e92f502d453b4
hu.yinbo <hu.hinbo@immomo.com>
2018-08-06 15:02:44 +0800

    添加：添加 BaseFragment and BaseAcitivity

---
+++ sample/src/main/java/com/example/momo/performance/BaseActivity.java
@@ -0,0 +1,17 @@
+package com.example.momo.performance;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by momo on 8/6/18.
+ */
+
+public class BaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
