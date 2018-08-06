f2d1044deb7235d294df02cd089e8dad77007765
hu.yinbo <hu.hinbo@immomo.com>
2018-08-06 14:57:17 +0800

    添加：添加 ui 卡顿的测试用例

---
+++ sample/src/main/java/com/example/momo/performance/DemoApplication.java
@@ -0,0 +1,15 @@
+package com.example.momo.performance;
+
+import android.app.Application;
+
+/**
+ * Created by momo on 8/6/18.
+ */
+
+public class DemoApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
